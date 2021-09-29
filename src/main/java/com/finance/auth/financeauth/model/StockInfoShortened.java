e8b22c5b069c1f89beb66d89700892c47f7411a1
bill0cypher <atakshievbogdan@gmail.com>
2021-09-30 01:02:55 +0300

    finance-loader-auth v.0.0.4

--- src/main/java/com/finance/auth/financeauth/model/StockInfoShortened.java
+++ src/main/java/com/finance/auth/financeauth/model/StockInfoShortened.java
@@ -1,9 +1,15 @@
 package com.finance.auth.financeauth.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "stockinfo")
+@Getter
+@Setter
+@NoArgsConstructor
 public class StockInfoShortened {
 
     @Id
@@ -16,81 +22,6 @@ public class StockInfoShortened {
     private Integer avgTotalVolume;
     private Double change;
     private Double changePercent;
-
-    public StockInfoShortened() {
-    }
-
-    public String getSymbol() {
-        return symbol;
-    }
-
-    public void setSymbol(String symbol) {
-        this.symbol = symbol;
-    }
-
-    public String getCompanyName() {
-        return companyName;
-    }
-
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
-
-    public Double getVolume() {
-        return volume;
-    }
-
-    public void setVolume(Double volume) {
-        this.volume = volume;
-    }
-
-    public Double getPreviousVolume() {
-        return previousVolume;
-    }
-
-    public void setPreviousVolume(Double previousVolume) {
-        this.previousVolume = previousVolume;
-    }
-
-    public Double getLatestVolume() {
-        return latestVolume;
-    }
-
-    public void setLatestVolume(Double latestVolume) {
-        this.latestVolume = latestVolume;
-    }
-
-    public Integer getAvgTotalVolume() {
-        return avgTotalVolume;
-    }
-
-    public void setAvgTotalVolume(Integer avgTotalVolume) {
-        this.avgTotalVolume = avgTotalVolume;
-    }
-
-    public Double getChange() {
-        return change;
-    }
-
-    public void setChange(Double change) {
-        this.change = change;
-    }
-
-    public Double getChangePercent() {
-        return changePercent;
-    }
-
-    public void setChangePercent(Double changePercent) {
-        this.changePercent = changePercent;
-    }
 
     @Override
     public String toString() {
--- src/main/java/com/finance/auth/financeauth/service/InstitutionService.java
+++ src/main/java/com/finance/auth/financeauth/service/InstitutionService.java
@@ -2,11 +2,13 @@ package com.finance.auth.financeauth.service;
 
 import com.finance.auth.financeauth.model.Institution;
 import com.finance.auth.financeauth.repo.InstitutionRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class InstitutionService {
 
     private final InstitutionRepository institutionRepository;
@@ -16,6 +18,7 @@ public class InstitutionService {
     }
 
     public List<Institution> findAll() {
+        log.info("Reading institutions...");
         return institutionRepository.findAll();
     }
 }
